6c527d36e6733d88b75ce4a70a16d6590e15f468
Enzo Vera <eevp88@gmail.com>
2025-07-25 13:38:39 -0400

    se agrega rutas post, put, delete

--- index.ts
+++ index.ts
@@ -5,6 +5,9 @@ const app = express();
 
 const port = config.SERVER.port || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", async (req, res) => {
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/users",
@@ -13,10 +16,26 @@ app.get("/", async (req, res) => {
   res.status(200).send(JSON.stringify(data));
 });
 
-app.post("/users", async (req, res) => {
+app.post("/", async (req, res) => {
   const newUser = req.body;
-  console.log(newUser);
-  res.status(201).send(JSON.stringify(newUser));
+  const { data } = await axios.post(
+    "https://jsonplaceholder.typicode.com/users",
+    newUser,
+  );
+  res.status(201).send(data);
+});
+
+app.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const newUser = req.body;
+  await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, newUser);
+  res.sendStatus(204);
+});
+
+app.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+  res.sendStatus(204);
 });
 
 app.listen(port, () => {
